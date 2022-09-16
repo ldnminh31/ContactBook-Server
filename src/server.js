da7c41fac7d0cca9145d02a172db8e9bd064c708
Minh Le <ledoannhatminh31@gmail.com>
2022-09-16 09:03:09 +0700

    Define routes for managing contacts

--- src/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-// start server
-const PORT = process.env.PORT;
-app.listen(PORT, () =>{
-    
-    console.log('Server is running on port ${PORT}',);
-});
